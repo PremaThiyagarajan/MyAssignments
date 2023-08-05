d35e2ba0227598e7bfbe109c73860934318de2aa
PremaThiyagarajan <140268168+PremaThiyagarajan@users.noreply.github.com>
2023-08-05 22:17:39 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,17 @@
+package org.system;
+
+public class Desktop extends Computer {
+
+	public void desktopsize() {
+		System.out.println("Desktop size: 32 inch");
+	}
+	public static void main(String[] args) {
+		Desktop obj= new Desktop();
+		obj.computerModel();
+		obj.desktopsize();
+				
+		
+
+	}
+
+}
